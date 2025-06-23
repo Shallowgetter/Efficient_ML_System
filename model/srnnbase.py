29475885788ccd90b04a4cc7f4c5de77d862ed1d
Shallowgetter <2076397996@sjtu.edu.cn>
2025-06-23 20:35:42 +0800

    custom neuron through SpikingJelly

---
+++ model/srnnbase.py
@@ -0,0 +1,12 @@
+"""
+SRNN using selected neurons.
+# -----------------------------------------------------------------------------
+Basic structure following paper 
+'Accurate and efficient time-domain classification with adaptive spiking recurrent neural networks' 
+@ https://arxiv.org/abs/2103.12593.
+
+Build SRNN model using spikingjelly and pytorch.
+# -----------------------------------------------------------------------------
+"""
+
+from __future__ import annotations
